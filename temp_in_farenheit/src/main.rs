3dc2eb69715d36cb4607eaf77c166492473fc6a2
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2023-05-01 02:55:48 +0100

    temp in farenheit

--- temp_in_farenheit/src/main.rs
+++ temp_in_farenheit/src/main.rs
@@ -1,11 +1,24 @@
 use std::io;
 
 fn main() {
-    println!("Please input a temperature in celcius to convert to farenheit:")
+    println!("Please input a temperature in celcius to convert to farenheit:");
 
-    let mut input = String new();
+    
+    loop {
+        let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line.")
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
+
+        let input: f64 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, 
+        };
+
+        println!("{}F", input * (5.0/9.0) + 32.0);
+        break;
+    }
+
+        
 }
